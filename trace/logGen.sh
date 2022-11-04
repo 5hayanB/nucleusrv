1570365d993b3b60c65ae3cf82c4022689d6c858
5hayanB <baig.sh@proton.me>
2022-11-04 16:20:56 +0500

    isolated burq related work

--- trace/logGen.sh
+++
@@ -1,9 +0,0 @@
-echo "Generating log..."
-cd ../
-sbt "testOnly nucleusrv.components.TopTest -- -DwriteVcd=1 -DprogramFile=./asm/assembly.hex" > ./trace/core.log
-echo "Log generated successfully"
-
-cd trace/
-echo "Generating CSV..."
-python ./toCSV.py
-echo "CSV generated successfully"
